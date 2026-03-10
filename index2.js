f6e53b4a96dd935e0435dc49c232b24a522e37ff
asadsftd <asadkhanopu6@gmail.com>
2026-03-10 13:32:24 -0700

    added the my all sections bloge commit

--- index2.js
+++ index2.js
@@ -2,6 +2,7 @@ document.getElementById("btn")
 .addEventListener('click', function(event){
     window.location.href="index.html";
 });
+
 document.getElementById("add")
 .addEventListener('click', function(event){
     const amount = document.getElementById("amount").value;
@@ -17,4 +18,35 @@ document.getElementById("add")
     else{
         console.log("thik nai");
     }
-})
+});
+
+document.getElementById("cash-out").style.display= "none";
+document.getElementById("card-one")
+.addEventListener('click', function(event){
+document.getElementById("add-money").style.display="block";
+document.getElementById("cash-out").style.display="none";
+});
+document.getElementById("card-two")
+.addEventListener('click', function(event){
+document.getElementById("add-money").style.display="none";
+document.getElementById("cash-out").style.display="block";
+});
+
+
+
+document.getElementById("out")
+.addEventListener('click', function(event){
+    const amount = document.getElementById("cashout-amount").value;
+    const convertedAmount = parseFloat(amount);
+    const mainBalance = document.getElementById("mainbalance").innerText;
+    const convertedBalance = parseFloat(mainBalance);
+    const pin = document.getElementById("cashout-pin").value;
+    const cenvertedPin = parseInt(pin);
+    if(cenvertedPin === 1234){
+        const sum = convertedBalance - convertedAmount;
+        document.getElementById("mainbalance").innerText=sum;
+    }
+    else{
+        console.log("thik nai");
+    }
+});

4f880c397988ba88500b9750d40ca9060739bb6a
asadsftd <asadkhanopu6@gmail.com>
2026-03-10 00:20:36 -0700

    added the my all add money bloge

--- index2.js
+++ index2.js
@@ -2,3 +2,19 @@ document.getElementById("btn")
 .addEventListener('click', function(event){
     window.location.href="index.html";
 });
+document.getElementById("add")
+.addEventListener('click', function(event){
+    const amount = document.getElementById("amount").value;
+    const convertedAmount = parseFloat(amount);
+    const mainBalance = document.getElementById("mainbalance").innerText;
+    const convertedBalance = parseFloat(mainBalance);
+    const pin = document.getElementById("pin").value;
+    const cenvertedPin = parseInt(pin);
+    if(cenvertedPin === 1234){
+        const sum = convertedAmount + convertedBalance;
+        document.getElementById("mainbalance").innerText=sum;
+    }
+    else{
+        console.log("thik nai");
+    }
+})
